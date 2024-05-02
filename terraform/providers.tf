76b92a5cf0bb9980c42f8b96441ec07fac6c50e4
Jekaterina Macilevic <jekaterinam@Jekaterinas-MacBook-Pro.local>
2024-05-02 11:35:26 +0100

    added variables in tf script

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -10,6 +10,10 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "${{ secrets.AZURE_SUBSCRIPTION_ID }}"
+  client_id       = "${{ secrets.AZURE_CLIENT_ID }}"
+  client_secret   = "${{ secrets.AZURE_CLIENT_SECRET }}"
+  tenant_id       = "${{ secrets.AZURE_TENANT_ID }}"
 }
 
 
