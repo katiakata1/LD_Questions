3171ca3c68afce20eda3d70415599af7e1c500bf
Jekaterina Macilevic <jekaterinam@Jekaterinas-MacBook-Pro.local>
2024-05-02 09:45:18 +0100

    I am happy with design

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime 
+from flask import redirect, url_for
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -15,6 +16,10 @@ class Entry(db.Model):
 def index():
     return render_template('index.html')
 
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
+
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
@@ -22,7 +27,7 @@ def submit():
         new_entry = Entry(content=entry_content)
         db.session.add(new_entry)
         db.session.commit()
-        return 'Entry submitted and saved to database successfully.'
+        return redirect(url_for('thankyou'))  # Redirect to the thank you page
 
 if __name__ == '__main__':
     with app.app_context():
