90268541d97b140790c0bd323075940ff4dd425d
Jekaterina Macilevic <jekaterinam@Jekaterinas-MacBook-Pro.local>
2024-05-03 16:31:52 +0100

    established connection between the app and Azure db

--- app/app.py
+++ app/app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime 
 from flask import redirect, url_for
+import pyodbc
+from sqlalchemy import create_engine
+import urllib
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+
+params = urllib.parse.quote_plus \
+(r'Driver={ODBC Driver 18 for SQL Server};Server=tcp:ldquestionssqlserver.database.windows.net,1433;Database=ld_sql_db;Uid=sql_admin@ldquestionssqlserver;Pwd=LD_P@ssw0rd123!_secret;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine_azure = create_engine(conn_str,echo=True)
+
+# Set the SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = str(engine_azure.url)
+
 db = SQLAlchemy(app)
 
 class Entry(db.Model):
