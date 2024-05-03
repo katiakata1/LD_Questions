a305d7f22fb61ff95a42945adaae7435b9f2a3e4
Jekaterina Macilevic <jekaterinam@Jekaterinas-MacBook-Pro.local>
2024-05-03 12:16:49 +0100

    deploying sql db

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,16 +13,16 @@
 # }
 
 // Deploying SQL
-# module "sql" {
-#     source = "./SQL_db"
-#     sql_server_name = var.sql_server_name
-#     rg_name = var.rg_name
-#     sql_server_version = var.sql_server_version
-#     administrator_login = var.administrator_login
-#     administrator_login_password = var.administrator_login_password
-#     sql_db_name = var.sql_db_name
-#     db_collation = var.db_collation
-# }
+module "sql" {
+    source = "./SQL_db"
+    sql_server_name = var.sql_server_name
+    rg_name = var.rg_name
+    sql_server_version = var.sql_server_version
+    administrator_login = var.administrator_login
+    administrator_login_password = var.administrator_login_password
+    sql_db_name = var.sql_db_name
+    db_collation = var.db_collation
+}
 
 
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -15,9 +15,9 @@ variable "tfstate_sa_name" {}
 variable "tfstate_container_name" {}
 
 // SQL Variables
-# variable "sql_server_name" {}
-# variable "sql_server_version" {}
-# variable "administrator_login" {}
-# variable "administrator_login_password" {}
-# variable "sql_db_name" {}
-# variable "db_collation" {}
+variable "sql_server_name" {}
+variable "sql_server_version" {}
+variable "administrator_login" {}
+variable "administrator_login_password" {}
+variable "sql_db_name" {}
+variable "db_collation" {}
