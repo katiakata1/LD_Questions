44a0259e8ac41d4c5a70243b0efd8b2034279af1
Jekaterina Macilevic <jekaterinam@Jekaterinas-MacBook-Pro.local>
2024-05-06 13:34:44 +0100

    collation problem saving russian letters

--- app/app.py
+++ app/app.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 
 params = urllib.parse.quote_plus \
 (r'Driver={ODBC Driver 18 for SQL Server};Server=tcp:ldquestionssqlserver.database.windows.net,1433;Database=ld_sql_db;Uid=sql_admin@ldquestionssqlserver;Pwd=LD_P@ssw0rd123!_secret;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
-conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+# conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+conn_str = f'mssql+pyodbc:///?odbc_connect={params};charset=utf8mb4'
 engine_azure = create_engine(conn_str,echo=True)
 
 # Set the SQLALCHEMY_DATABASE_URI
@@ -20,7 +21,7 @@ db = SQLAlchemy(app)
 
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(255), nullable=False)
+    content = db.Column(db.UnicodeText(collation='utf8mb4_general_ci'), nullable=False)  # Use UnicodeText to support longer strings
     created_at = db.Column(db.DateTime, default=datetime.utcnow) 
 
 @app.route('/')
@@ -35,6 +36,7 @@ def thankyou():
 def submit():
     if request.method == 'POST':
         entry_content = request.form['entry']
+        # entry_content_utf8 = entry_content.encode('utf-8')
         new_entry = Entry(content=entry_content)
         db.session.add(new_entry)
         db.session.commit()
@@ -43,4 +45,4 @@ def submit():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(port=5000, host='0.0.0.0', debug=True)
+    app.run(port=5001, host='0.0.0.0', debug=True)
