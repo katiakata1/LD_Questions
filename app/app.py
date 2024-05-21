bd82604994a39b3e9a553bcdee29a599ef118df3
Jekaterina Macilevic <jekaterinam@Jekaterinas-MacBook-Pro.local>
2024-05-21 11:23:34 +0100

    changed some design

--- app/app.py
+++ app/app.py
@@ -45,4 +45,4 @@ def submit():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(port=5000, host='0.0.0.0', debug=True)
+    app.run(port=5001, host='0.0.0.0', debug=True)
