59c91ddc45a2d27ee162f18bb0662e88a263d058
Jekaterina Macilevic <jekaterinam@jekaterinas-mbp.home>
2024-07-30 21:19:51 +0100

    forgot to add data. to rg name and location

--- terraform/App_Insights/main.tf
+++ terraform/App_Insights/main.tf
@@ -4,8 +4,8 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_application_insights" "app_insights" {
   name                = var.app_insights_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   application_type    = "web"
 }
 
