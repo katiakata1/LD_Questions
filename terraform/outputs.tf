78544f5759b8683118c1039357bd0a5b92d81878
Jekaterina Macilevic <jekaterinam@jekaterinas-mbp.home>
2024-07-31 11:17:15 +0100

    terraform is fine

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,7 +2,7 @@ output container_app_ip_address {
     value = module.container_app.container_app_ip_address
 }
 
-output app_insights_instrumentation_key {
-    value = module.app_insights.app_insights_instrumentation_key
-    sensitive = true
-}
+# output app_insights_instrumentation_key {
+#     value = module.app_insights.app_insights_instrumentation_key
+#     sensitive = true
+# }
